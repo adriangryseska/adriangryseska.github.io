892b6a0652e9954169fbcff6ba55a413403f8d79
adriangryseska <85684462+adriangryseska@users.noreply.github.com>
2022-03-02 21:54:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
   } else {
-    document.getElementById("header").style.top = "-50px";
+    document.getElementById("header").style.top = "-70px";
   }
   prevScrollpos = currentScrollPos;
 } 
